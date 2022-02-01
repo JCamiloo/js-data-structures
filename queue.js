4dfad807c1470903e4516352d69847c3577fd285
JCamiloo <jcamilo.osorio15@gmail.com>
2022-02-01 15:25:57 -0500

    feat(queue): adding basic implementation

---
+++ queue.js
@@ -0,0 +1,51 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class MyQueue {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.length = 0;
+  }
+
+  peek() {
+    return this.first;
+  }
+
+  enqueue(value) {
+    const newNode = new Node(value);
+
+    if (this.length === 0) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      this.last.next = newNode;
+      this.last = newNode;
+    }
+
+    this.length++;
+
+    return this;
+  }
+
+  dequeue() {
+    if (this.length === 0) {
+      return;
+    }
+
+    this.first = this.first.next;
+    this.length--;
+
+    if (this.length === 0) {
+      this.last = null;
+    }
+
+    return this;
+  }
+}
+
+const myQueue = new MyQueue();
